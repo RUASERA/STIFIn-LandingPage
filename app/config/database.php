4328900a75ab8ab78bdea1524b588bb8f95eda78
0rcaaa <iamsyarel@gmail.com>
2025-10-09 09:52:16 +0700

    /fix wrong variabels

--- app/config/database.php
+++ app/config/database.php
@@ -5,10 +5,10 @@ $dot = parse_ini_file(__DIR__ . '/.env');
 
 $conn = new mysqli(
   $dot['DB_HOST'],
-  $dot['DB_HOST'],
-  $dot['DB_HOST'],
-  $dot['DB_HOST'],
-  $dot['DB_HOST'],
+  $dot['DB_USERNAME'],
+  $dot['DB_PASSWORD'],
+  $dot['DB_DATABSE'],
+  $dot['DB_PORT'],
 );
 
 if ($conn->connect_error) {
