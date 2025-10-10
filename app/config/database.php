b9be44d143cb10aacfcbb270e6c4ed5f241c8d4d
tyonis72 <danishraihan05@gmail.com>
2025-10-10 15:08:50 +0700

    /add sert management system interface

--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,6 @@
 <?php
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $dot = parse_ini_file(__DIR__.'/../../.env');
 
 $conn = new mysqli(
