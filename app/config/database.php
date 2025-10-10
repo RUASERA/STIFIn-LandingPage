d63368ad94f8cd766bbc543adc720b531daf3e61
0rcaaa <iamsyarel@gmail.com>
2025-10-10 14:15:20 +0700

    /fix database path

--- app/config/database.php
+++ app/config/database.php
@@ -1,24 +1,23 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require './utils.php';
-$dot = parse_ini_file('../../.env');
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+$dot = parse_ini_file(__DIR__.'/../../.env');
 
 $conn = new mysqli(
   $dot['DB_HOST'],
   $dot['DB_USERNAME'],
   $dot['DB_PASSWORD'],
   $dot['DB_DATABASE'],
-  $dot['DB_PORT'],
+  $dot['DB_PORT']
 );
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'PUT' || $_SERVER['REQUEST_METHOD'] == 'DELETE' ) {
-    echo json_encode(['error' => 'Unauthorized']);
-    http_response_code(403);
-    exit();
-}
+// if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'PUT' || $_SERVER['REQUEST_METHOD'] == 'DELETE' ) {
+//     echo json_encode(['error' => 'Unauthorized']);
+//     http_response_code(403);
+//     exit();
+// }
 
 if ($conn->connect_error) {
   die("Koneksi gagal: " . $conn->connect_error);
 }
-echo "Koneksi berhasil!";
+// echo "Koneksi berhasil!";
